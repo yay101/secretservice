781bb285732239147db60ef4cf84f9082ca25bc4
yay101 <yay101@gmail.com>
2023-01-28 00:52:04 +1100

    - Some html tidying

--- web/index.js
+++ web/index.js
@@ -60,11 +60,13 @@ app = {
             break;
             case "audio":
                 app.form.style = "display:flex;"
-                app.form.innerHTML = `<button class="control" id="playback" onclick="app.media.play()"><img class="icon" src="/img/play.svg">Play</button><button class="control" id="record" onclick="app.media.start()"><img class="icon" src="/img/record2.svg">Record</button><button class="control" onclick="app.media.cancel()"><img class="icon" src="/img/eject.svg">Cancel</button>`;
+                app.form.innerHTML = `<audio id="audio"></audio><button class="control" id="playback" onclick="app.media.play()"><img class="icon" src="/img/play.svg">Play</button><button class="control" id="record" onclick="app.media.start()"><img class="icon" src="/img/record2.svg">Record</button><button class="control" onclick="app.media.cancel()"><img class="icon" src="/img/eject.svg">Cancel</button>`;
+                app.media.mediaPlayer = document.getElementById("audio");
             break;
             case "video":
-                app.form.style = "display:flex;margin:auto;"
-                app.form.innerHTML = `<video id="videomonitor" style="min-width:12rem;margin:auto;" autoplay></video><div><button class="control" id="playback" onclick="app.media.play()"><img class="icon" src="/img/play.svg">Play</button><button class="control" id="record" onclick="app.media.start()"><img class="icon" src="/img/record2.svg">Record</button><button class="control" onclick="app.media.cancel()"><img class="icon" src="/img/eject.svg">Cancel</button></div>`;
+                app.form.style = "display:flex;"
+                app.form.innerHTML = `<video id="video" style="min-width:12rem;margin:auto;width:100%;" autoplay controls></video><div id="controls" style="display:flex;"><button class="control" id="playback" onclick="app.media.play()"><img class="icon" src="/img/play.svg">Play</button><button class="control" id="record" onclick="app.media.start()"><img class="icon" src="/img/record2.svg">Record</button><button class="control" onclick="app.media.cancel()"><img class="icon" src="/img/eject.svg">Cancel</button></div>`;
+                app.media.mediaPlayer = document.getElementById("video");
             break;
         }
     },
@@ -85,50 +87,32 @@ app = {
         mediaPlayer: null,
         mediaBlob: null,
         play() {
-            switch(document.getElementById("type").value){
-                case "audio":
-                    this.mediaPlayer = document.createElement("audio");
-                    this.mediaPlayer.src = URL.createObjectURL(this.mediaBlob)
-                    this.mediaPlayer.play()
-                break;
-                case "video":
-                    this.mediaPlayer = document.getElementById("videomonitor");
-                    this.mediaPlayer.src = URL.createObjectURL(this.mediaBlob)
-                    this.mediaPlayer.play()
-                break;
-            }
+            this.mediaPlayer.src = URL.createObjectURL(this.mediaBlob)
+            this.mediaPlayer.play()
         },
         start() {
             if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
                 return Promise.reject(new Error('mediaDevices API or getUserMedia method is not supported in this browser.'));
             }
             else {
-                document.getElementById("record").onclick = ()=>{app.media.stop()}
-                document.getElementById("record").innerHTML = `<img class="icon" src="/img/stop.svg">Stop`
-                switch(document.getElementById("type").value){
-                    case "audio":
-                        return navigator.mediaDevices.getUserMedia({ audio: true })
-                        .then(stream => {
-                            this.streamBeingCaptured = stream;
-                            this.mediaRecorder = new MediaRecorder(stream);
-                            this.mediaBlobs = [];
-                            this.mediaRecorder.addEventListener("dataavailable", event => {
-                                this.mediaBlobs.push(event.data);
-                            });
-                            this.mediaRecorder.start();
-                        });
-                    case "video":
-                        return navigator.mediaDevices.getUserMedia({ audio: true, video: true})
-                        .then(stream => {
-                            this.streamBeingCaptured = stream;
-                            this.mediaRecorder = new MediaRecorder(stream);
-                            this.mediaBlobs = [];
-                            this.mediaRecorder.addEventListener("dataavailable", event => {
-                                this.mediaBlobs.push(event.data);
-                            });
-                            this.mediaRecorder.start();
-                        });
+                var options
+                if(document.getElementById("type").value == "audio"){
+                    options = { audio: true }
+                } else {
+                    options = { audio: true, video: true}
                 }
+                return navigator.mediaDevices.getUserMedia(options)
+                .then(stream => {
+                    this.streamBeingCaptured = stream;
+                    this.mediaRecorder = new MediaRecorder(stream);
+                    this.mediaBlobs = [];
+                    this.mediaRecorder.addEventListener("dataavailable", event => {
+                        this.mediaBlobs.push(event.data);
+                    });
+                    this.mediaRecorder.start();
+                    document.getElementById("record").onclick = ()=>{app.media.stop()}
+                    document.getElementById("record").innerHTML = `<img class="icon" src="/img/stop.svg">Stop`
+                });
             }
         },
         stop() {
